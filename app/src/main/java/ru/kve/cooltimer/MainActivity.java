510a67477c4e3261b62067df7e9b7f6a74c39989
k.vedeneev <k.vedeneev@crif.com>
2020-03-04 17:24:39 +0300

    Seek bar and timer set.
    Signed-off-by: k.vedeneev <k.vedeneev@crif.com>

--- app/src/main/java/ru/kve/cooltimer/MainActivity.java
+++ app/src/main/java/ru/kve/cooltimer/MainActivity.java
@@ -1,14 +1,49 @@
 package ru.kve.cooltimer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.SeekBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+  private CountDownTimer timer;
+  private SeekBar seekBar;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    seekBar = findViewById(R.id.seekBar);
+    seekBar.setProgress(0);
+    seekBar.setMax(3600000);
+    seekBar.setProgress(60);
+
+//    seekBar.post(new Runnable() {
+//      @Override
+//      public void run() {
+//        seekBar.setProgress(60);
+//      }
+//    });
+
+    timer = new CountDownTimer(3600000, 1000) {
+      @Override
+      public void onTick(long millisUntilFinished) {
+        if (seekBar.getProgress() > 0) {
+          seekBar.setProgress(seekBar.getProgress() - 1);
+        } else {
+          timer.cancel();
+        }
+      }
+
+      @Override
+      public void onFinish() {
+
+      }
+    };
+
+    // timer.start();
   }
 }
