63bd53014fb53f235c766f324879ba62a022ef1c
k.vedeneev <k.vedeneev@crif.com>
2020-03-05 10:52:05 +0300

    Start timer.
    Signed-off-by: k.vedeneev <k.vedeneev@crif.com>

--- app/src/main/java/ru/kve/cooltimer/MainActivity.java
+++ app/src/main/java/ru/kve/cooltimer/MainActivity.java
@@ -2,7 +2,9 @@ package ru.kve.cooltimer;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,16 +12,38 @@ public class MainActivity extends AppCompatActivity {
 
   private CountDownTimer timer;
   private SeekBar seekBar;
+  private TextView textView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    textView = findViewById(R.id.textView);
     seekBar = findViewById(R.id.seekBar);
-    seekBar.setProgress(0);
-    seekBar.setMax(3600000);
-    seekBar.setProgress(60);
+    seekBar.setMax(600000);
+    seekBar.setProgress(60000);
+
+    seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        int min = progress / 1000 / 60;
+        int sec = progress / 1000 % 60;
+        String time = (min < 10 ? "0" : "") + String.valueOf(min) + ":" +
+            (sec < 10 ? "0" : "") + String.valueOf(sec);
+        textView.setText(time);
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+        // not used
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+        // not used
+      }
+    });
 
 //    seekBar.post(new Runnable() {
 //      @Override
@@ -28,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 //      }
 //    });
 
-    timer = new CountDownTimer(3600000, 1000) {
+    timer = new CountDownTimer(600000, 1000) {
       @Override
       public void onTick(long millisUntilFinished) {
         if (seekBar.getProgress() > 0) {
@@ -45,5 +69,15 @@ public class MainActivity extends AppCompatActivity {
     };
 
     // timer.start();
+  }
+
+  public void onClickStart(View view) {
+    if (((TextView) view).getText().equals("START")) {
+      ((TextView) view).setText("STOP");
+      timer.start();
+    } else {
+      ((TextView) view).setText("START");
+      timer.cancel();
+    }
   }
 }
