57ef1f75d363bf7c4264f684a27acb8bbcaf6aa8
k.vedeneev <k.vedeneev@crif.com>
2020-03-06 10:45:31 +0300

    All timer functions are realized.
    Finish ringing not supported yet.
    
    Signed-off-by: k.vedeneev <k.vedeneev@crif.com>

--- app/src/main/java/ru/kve/cooltimer/MainActivity.java
+++ app/src/main/java/ru/kve/cooltimer/MainActivity.java
@@ -3,6 +3,7 @@ package ru.kve.cooltimer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -13,22 +14,24 @@ public class MainActivity extends AppCompatActivity {
   private CountDownTimer timer;
   private SeekBar seekBar;
   private TextView textView;
+  private Button buttonStart;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    buttonStart = findViewById(R.id.buttonStart);
     textView = findViewById(R.id.textView);
     seekBar = findViewById(R.id.seekBar);
-    seekBar.setMax(600000);
-    seekBar.setProgress(60000);
+    seekBar.setMax(600);
+    seekBar.setProgress(60);
 
     seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
       @Override
       public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        int min = progress / 1000 / 60;
-        int sec = progress / 1000 % 60;
+        int min = progress / 60;
+        int sec = progress % 60;
         String time = (min < 10 ? "0" : "") + String.valueOf(min) + ":" +
             (sec < 10 ? "0" : "") + String.valueOf(sec);
         textView.setText(time);
@@ -45,38 +48,33 @@ public class MainActivity extends AppCompatActivity {
       }
     });
 
-//    seekBar.post(new Runnable() {
-//      @Override
-//      public void run() {
-//        seekBar.setProgress(60);
-//      }
-//    });
-
     timer = new CountDownTimer(600000, 1000) {
       @Override
       public void onTick(long millisUntilFinished) {
         if (seekBar.getProgress() > 0) {
           seekBar.setProgress(seekBar.getProgress() - 1);
         } else {
+          buttonStart.setText("START");
+          seekBar.setEnabled(true);
           timer.cancel();
         }
       }
 
       @Override
       public void onFinish() {
-
+        // not used
       }
     };
-
-    // timer.start();
   }
 
   public void onClickStart(View view) {
     if (((TextView) view).getText().equals("START")) {
       ((TextView) view).setText("STOP");
+      seekBar.setEnabled(false);
       timer.start();
     } else {
       ((TextView) view).setText("START");
+      seekBar.setEnabled(true);
       timer.cancel();
     }
   }
